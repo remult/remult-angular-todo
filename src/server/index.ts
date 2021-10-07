dcd2321b4fa30be6425149eb0bef4708f9d5f3be
Noam <noam@fireflymigration.com>
2021-10-07 06:17:45 +0300

    setup-deployment

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,13 +1,21 @@
 import * as express from 'express';
 import * as expressJwt from 'express-jwt';
 import { getJwtTokenSignKey } from '../app/auth.service';
+import * as compression from 'compression';
+import * as helmet from 'helmet';
 import { initExpress } from 'remult/server';
 
 let app = express();
+app.use(helmet({ contentSecurityPolicy: false }));
+app.use(compression());
 app.use(expressJwt({
     secret: getJwtTokenSignKey(),
     credentialsRequired: false,
     algorithms: ['HS256']
 }));
 initExpress(app);
-app.listen(3002, () => console.log("Server started"));
+app.use(express.static('dist/remult-angular-todo'));
+app.use('/*', async (req, res) => {
+   res.sendFile('./dist/remult-angular-todo/index.html');
+});
+app.listen(process.env.PORT || 3002, () => console.log("Server started"));
