e1e2b98ccf38babcababd146ab6766dff11f6d4e
Noam Honig <noam.honig@gmail.com>
2021-12-25 19:42:08 +0200

    Updated to latest angular

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,6 @@
 import * as express from 'express';
 import { remultExpress } from 'remult/remult-express';
 
-let app = express();
+const app = express();
 app.use(remultExpress());
 app.listen(3002, () => console.log("Server started"));
